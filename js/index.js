b09ae4666de55c043156882ef699cd74795e182a
lalalal99 <francesco.bucci99@hotmail.com>
2023-07-18 23:48:01 +0200

    index.js added

---
+++ js/index.js
@@ -0,0 +1,35 @@
+user = "lalalal99";
+root = "https://" + user + ".github.io";
+
+repos = [];
+
+function httpGetAsync(url, callback) {
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.onreadystatechange = function () {
+    if (xmlHttp.readyState == 4 && xmlHttp.status == 200) callback(JSON.parse(xmlHttp.responseText));
+  };
+  xmlHttp.open("GET", url, true); // true for asynchronous
+  xmlHttp.send(null);
+}
+
+function func(list) {
+  repo = list[5];
+  //   list.forEach((repo) => {
+  newUrl = root + "/" + repo.name;
+  console.log(newUrl + " " + UrlExists(newUrl));
+  //   });
+}
+
+function getRepos() {
+  url = "https://api.github.com/users/" + user + "/repos";
+  httpGetAsync(url, func);
+}
+
+function UrlExists(url) {
+  var http = new XMLHttpRequest();
+  http.open("HEAD", url, false);
+  http.send();
+  return http.status != 404;
+}
+
+getRepos();
